fb16319f94c2e79aa44e268eacebcbe1cf26a0d5
vedant2709 <vedantchaudhary2002@gmail.com>
2025-09-13 13:57:16 +0530

    Create custom utility classes for error and response. Also create asyncHandler function.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./.env",
 });
 
-connectDB();
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running at PORT ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connection failed !!!", err);
+  });
